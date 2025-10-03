873dbfda68a01cc0e13d86f4a0abf3b4517f16e4
Irina Dominte <scurtuirina@gmail.com>
2025-10-03 12:09:27 +0300

    starting point

--- NSBMessaging/Products.Domain/Entities/Product.cs
+++ NSBMessaging/Products.Domain/Entities/Product.cs
@@ -7,6 +7,7 @@ namespace Products.Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string LongDescription { get; set; }
